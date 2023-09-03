8fe725aaa45a6865499998d36769875438ae4e2c
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-03 13:34:11 +0000

    Initial Commit

---
+++ flickster/flicksterApp.swift
@@ -0,0 +1,17 @@
+//
+//  flicksterApp.swift
+//  flickster
+//
+//  Created by Michael Dadzie on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct flicksterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
