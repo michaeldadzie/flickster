a09558158297d039ed6353904a81c09021d66604
Michael Dadzie <dadziemikke2@gmail.com>
2023-09-06 13:27:47 +0000

    added subverse view

---
+++ flickster/Views/SubverseView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SubverseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubverseView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubverseView()
+    }
+}
