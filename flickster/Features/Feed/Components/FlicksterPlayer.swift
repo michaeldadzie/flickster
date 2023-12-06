95cacec3ad302d7a8254ffa9d6d2719025c375b0
Michael Dadzie <dadziemikke2@gmail.com>
2023-12-06 19:14:31 +0000

    feat: implement custom video player

---
+++ flickster/Features/Feed/Components/FlicksterPlayer.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+import AVKit
+import SwiftUI
+
+struct FlicksterPlayer: UIViewControllerRepresentable {
+    var player: AVPlayer
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        let controller = AVPlayerViewController()
+        controller.player = player
+        controller.showsPlaybackControls = false
+        controller.exitsFullScreenWhenPlaybackEnds = true
+        controller.allowsPictureInPicturePlayback = true
+        controller.videoGravity = .resizeAspectFill
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
--- flickster/Features/Feed/View/FeedView.swift
+++ flickster/Features/Feed/View/FeedView.swift
@@ -1,40 +1,82 @@
+import AVKit
 import SwiftUI
 
 struct FeedView: View {
+    @StateObject var viewModel = FeedViewModelImpl(service: FeedServiceImpl())
+    @StateObject var manager = FlicksterManager()
+    @State private var scrollPosition: Int?
+    // @State private var player = AVPlayer()
+    @State private var hasLoadedPosts = false
     
-    @StateObject var feedView = FeedViewModelImpl(service: FeedServiceImpl())
-    @StateObject var videoView = VideoPlayerViewModel()
+    // @StateObject var videoView = VideoPlayerViewModel()
     
     var body: some View {
-        NavigationView {
-            Group {
-                switch feedView.state {
-                case .loading:
-                    ZStack { }
-                case .failed(error: let error):
-                    Text(error.localizedDescription)
-                default:
-                    ZStack {
-                        PlayerScrollView(videoPlayerViewModel: videoView, post: feedView.posts)
-                    }
+        ScrollView {
+            LazyVStack(spacing: 0) {
+                ForEach(viewModel.posts) { post in
+                    FeedCell(post: post, player: manager.player)
+                        .id(post.id)
+                        .onAppear {
+                            initPlayer()
+                        }
                 }
             }
-            .onTapGesture {
-                if (videoView.isPlaying) {
-                    videoView.pauseVideo()
-                } else {
-                    videoView.currentPlayer?.play()
-                }
-            }
-            .ignoresSafeArea(.all)
-            .background(.black)
-            .onAppear(perform: feedView.getPosts)
+            .scrollTargetLayout()
         }
+        .onAppear {
+            manager.player.play()
+        }
+        .scrollPosition(id: $scrollPosition)
+        .scrollTargetBehavior(.paging)
+        .ignoresSafeArea()
+        .onChange(of: scrollPosition) { previousIndex, currentIndex in
+            print(currentIndex!)
+            onChanged(postId: currentIndex)
+        }
+        .task(priority: .background, { loadPostsIfNeeded() })
+         /* NavigationView {
+            Group {
+                switch viewModel.state {
+                case .loading:
+                    ZStack {
+                        Color(.white)
+                        ProgressView()
+                    }
+                    
+                default:
+                    ScrollView {}
+                }
+            }
+            .task(priority: .background, { viewModel.getPosts() })
+        } */
+    }
+    
+    private func loadPostsIfNeeded() {
+        guard !hasLoadedPosts else {
+            return
+        }
+        
+        viewModel.getPosts()
+        hasLoadedPosts = true
+    }
+    
+    func initPlayer() {
+        guard scrollPosition == nil, let post = viewModel.posts.first,
+              manager.player.currentItem == nil else { return }
+        
+        let item = AVPlayerItem(url: URL(string: post.videoLink)!)
+        manager.player.replaceCurrentItem(with: item)
+    }
+    
+    func onChanged(postId: Int?) {
+        guard let currentIndex = viewModel.posts.first(where: {  $0.id == postId }) else { return }
+        
+        manager.player.replaceCurrentItem(with: nil) // wipes out previous player, figure out a way pause for 3 indexs before replacing
+        let playerItem = AVPlayerItem(url: URL(string: currentIndex.videoLink)!)
+        manager.player.replaceCurrentItem(with: playerItem)
     }
 }
 
-struct FeedView_Previews: PreviewProvider {
-    static var previews: some View {
-        FeedView()
-    }
+#Preview {
+    FeedView()
 }
