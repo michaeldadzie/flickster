5e97e8e9f0ec57dfb14d8f91a65c55d160014c77
Michael Dadzie <dadziemikke2@gmail.com>
2023-12-06 19:11:46 +0000

    improve: update tabview ui

--- flickster/Core/TabView/FlicksterTabView.swift
+++ flickster/Core/TabView/FlicksterTabView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 import UIKit
 
 struct FlicksterTabView: View {
+    @State private var selectedTab = 0
+    @StateObject var manager = FlicksterManager()
     
     init() {
         UITabBar.appearance().unselectedItemTintColor = .gray
@@ -9,29 +11,40 @@ struct FlicksterTabView: View {
     }
     
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             FeedView()
                 .tabItem {
-                    Image(systemName: "house")
-                    Text("Feed")
+                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                    Text("Home")
                 }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
             
             SubverseView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                     Text("Subverse")
                 }
-                .onTapGesture {
-                    
+                .onAppear { 
+                    selectedTab = 1
                 }
+                .tag(1)
+                // .onTapGesture { manager.player.pause() }
             
             ProfileView()
                 .tabItem {
-                    Image(systemName: "person")
-                    Text("Profile")
+                    Image(systemName: selectedTab == 2 ? "person.fill" : "person")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                    Text("Profile ")
                 }
+                .onAppear { 
+                    selectedTab = 2
+                }
+                .tag(2)
+                // .onTapGesture { manager.player.pause() }
         }
-        .accentColor(.red)
+        .tint(.red)
     }
 }
 
