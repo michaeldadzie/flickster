e143be89f6f6ae29aa4a8173c3929024201319d2
Michael Dadzie <dadziemikke2@gmail.com>
2024-01-14 16:41:49 +0000

    update: change post data struct name

--- flickster/Features/Feed/Service/FeedService.swift
+++ flickster/Features/Feed/Service/FeedService.swift
@@ -2,20 +2,17 @@ import Combine
 import Foundation
 
 protocol FeedService {
-    func request(from endpoint: FeedAPI) -> AnyPublisher<PostData, APIError>
+    func request(from endpoint: FeedAPI) -> AnyPublisher<PostModel, APIError>
 }
 
 struct FeedServiceImpl: FeedService {
-    func request(from endpoint: FeedAPI) -> AnyPublisher<PostData, APIError> {
-        
+    func request(from endpoint: FeedAPI) -> AnyPublisher<PostModel, APIError> {
         return URLSession
             .shared
             .dataTaskPublisher(for: endpoint.urlRequest)
             .receive(on: DispatchQueue.main)
             .mapError {_ in APIError.unknown }
-            .flatMap { data, response -> AnyPublisher<PostData, APIError> in
-                // print(endpoint.urlRequest)
-                // print(String(data: data, encoding: .utf8)!)
+            .flatMap { data, response -> AnyPublisher<PostModel, APIError> in
                 guard let response = response as? HTTPURLResponse else {
                     return Fail(error: APIError.unknown).eraseToAnyPublisher()
                 }
@@ -24,7 +21,7 @@ struct FeedServiceImpl: FeedService {
                     let jsonDecoder = JSONDecoder()
                     jsonDecoder.dataDecodingStrategy = .base64
                     return Just(data)
-                        .decode(type: PostData.self, decoder: jsonDecoder)
+                        .decode(type: PostModel.self, decoder: jsonDecoder)
                         .mapError { error in
                             print("Decoding error: \(error)")
                             return APIError.decodingError }
