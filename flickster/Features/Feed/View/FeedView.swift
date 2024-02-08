fa36799321a7405eb0d4843a70461c71ad69189d
Michael Dadzie <dadziemikke2@gmail.com>
2024-02-08 19:53:51 +0000

    refactor: minor code refactor

--- flickster/Features/Feed/View/FeedView.swift
+++ flickster/Features/Feed/View/FeedView.swift
@@ -1,28 +1,33 @@
-import AVKit
 import SwiftUI
 
 struct FeedView: View {
-    //@StateObject var manager = FlicksterManager(viewModel: FeedViewModelImpl(service: FeedServiceImpl()))
-    @StateObject var viewModel = FeedViewModelImpl(service: FeedServiceImpl())
-    @State private var hasLoadedPosts = false
+    @StateObject var viewModel = FeedViewModel(service: FeedService())
     
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 0) {
-                ForEach(viewModel.posts) { post in
-                    FeedCell(post: post)
-                        .id(post.id)
+        ZStack {
+            #if !os(visionOS)
+            Color.black
+                .ignoresSafeArea()
+            #endif
+            
+            ScrollView {
+                LazyVStack(spacing: 0) {
+                    ForEach(viewModel.posts) { post in
+                        FeedCell(post: post).id(post.id)
+                    }
                 }
+                .scrollTargetLayout()
             }
-            .scrollTargetLayout()
-        }
-        .scrollIndicators(.hidden)
-        .scrollPosition(id: $viewModel.scrollPosition)
-        .scrollTargetBehavior(.paging)
-        .ignoresSafeArea()
-        .task { await viewModel.getPosts() }
-        .onChange(of: viewModel.scrollPosition) { previousIndex, currentIndex in
-            viewModel.onChanged(previousID: previousIndex, currentID: currentIndex)
+            .scrollIndicators(.hidden)
+            .scrollPosition(id: $viewModel.scrollPosition)
+            .scrollTargetBehavior(.paging)
+            .ignoresSafeArea()
+            //.task { await viewModel.getPosts() }
+            .onAppear {Task { await viewModel.getPosts() }}
+            .onChange(of: viewModel.scrollPosition) { previousIndex, currentIndex in
+                viewModel.onChanged(previousID: previousIndex, currentID: currentIndex)
+            }
+            
         }
     }
 }
