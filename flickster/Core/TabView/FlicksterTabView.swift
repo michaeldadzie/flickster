d5dfb18ac678f3aff9ca92ba110384976a1b0afd
Michael Dadzie <dadziemikke2@gmail.com>
2024-02-08 19:50:09 +0000

    update: add new tabs

--- flickster/Core/TabView/FlicksterTabView.swift
+++ flickster/Core/TabView/FlicksterTabView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import UIKit
 
 struct FlicksterTabView: View {
-    @EnvironmentObject var launchViewModel: LaunchScreenViewModel
+    @EnvironmentObject var launchViewModel: LaunchViewModel
     @State private var selectedTab = 0
     
     init() {
@@ -24,24 +24,39 @@ struct FlicksterTabView: View {
             
             SubverseView()
                 .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("Search")
+                    Image(systemName: selectedTab == 1 ? "person.2.fill" : "person.2")
+                        .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                    Text("Friends")
                 }
-                .onAppear {
-                    selectedTab = 1
-                }
+                .onAppear { selectedTab = 1 }
                 .tag(1)
+            
+            SubverseView()
+                .tabItem {
+                    Image(systemName: selectedTab == 2 ? "plus.app.fill" : "plus.app")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                    Text("Post")
+                }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
+            
+            SubverseView()
+                .tabItem {
+                    Image(systemName: selectedTab == 3 ? "text.bubble.fill" : "text.bubble")
+                        .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                    Text("Inbox")
+                }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
             
             ProfileView()
                 .tabItem {
-                    Image(systemName: selectedTab == 2 ? "person.fill" : "person")
-                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                    Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                        .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
                     Text("Profile")
                 }
-                .onAppear {
-                    selectedTab = 2
-                }
-                .tag(2)
+                .onAppear { selectedTab = 4 }
+                .tag(4)
         }
         .onAppear {
             DispatchQueue
